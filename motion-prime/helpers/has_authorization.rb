e32dbec11a56223ea6b70262ccb6b3b7a52fd682
Iskander Haziev <gvalmon@gmail.com>
2013-07-29 00:14:51 +0400

    update has authorization module

--- motion-prime/helpers/has_authorization.rb
+++ motion-prime/helpers/has_authorization.rb
@@ -1,10 +1,15 @@
 module MotionPrime
   module HasAuthorization
     def current_user
-      @current_user = User.current
+      if defined?(User) && User.respond_to?(:current)
+        @current_user = User.current
+      end
+    end
+    def user_signed_in?
+      current_user.present?
     end
     def api_client
-      @api_client ||= ApiClient.new(access_token: current_user.access_token)
+      @api_client ||= ApiClient.new(access_token: current_user.try(:access_token))
     end
   end
 end
