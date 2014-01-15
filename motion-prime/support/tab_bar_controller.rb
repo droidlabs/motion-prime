8ffd7d79f42b3daf3b217d82017c04514b618218
Iskander Haziev <gvalmon@gmail.com>
2014-01-15 12:26:24 +0400

    added selected image support to tab bar item

--- motion-prime/support/tab_bar_controller.rb
+++ motion-prime/support/tab_bar_controller.rb
@@ -5,14 +5,11 @@ module MotionPrime
 
       view_controllers = []
 
-      screens.each_with_index do |screen, index|
-        if screen.is_a?(Hash)
-          screen, image, title = screen[:screen], screen[:image], screen[:title]
-          screen = Screen.create_with_options(screen).try(:weak_ref)
-          title ||= screen.title
-          image = image.uiimage if image
-          screen.tabBarItem = UITabBarItem.alloc.initWithTitle title, image: image, tag: index
+      screens.each_with_index do |options, index|
+        if options.is_a?(Hash)
+          screen = init_screen_with_options(options, tag: index)
         else
+          screen = options
           screen.tabBarItem.tag = index
         end
 
@@ -33,5 +30,29 @@ module MotionPrime
       end
       controller
     end
+
+    protected
+      def self.init_screen_with_options(options, tag: tag)
+        screen, image, title = options[:screen], options[:image], options[:title]
+        screen = Screen.create_with_options(screen).try(:weak_ref)
+        title ||= screen.title
+
+        image = extract_image_from_options(options, with_key: :image)
+        screen.tabBarItem = UITabBarItem.alloc.initWithTitle title, image: image, tag: tag
+
+        selected_image = extract_image_from_options(options, with_key: :selected_image)
+        screen.tabBarItem.setSelectedImage(selected_image) if selected_image
+        screen
+      end
+
+      def self.extract_image_from_options(options, with_key: key)
+        image = options[key]
+        return unless image
+        image = image.uiimage
+        if options[:translucent] === false
+          image = image.imageWithRenderingMode UIImageRenderingModeAlwaysOriginal
+        end
+        image
+      end
   end
 end
