127727a076e4e6ced4aa192efc5ae7906e827a77
Iskander Haziev <gvalmon@gmail.com>
2014-10-19 19:30:45 +0400

    fix

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ sudo chown -R travis ~/Library/RubyMotion
 mkdir -p ~/Library/RubyMotion/build
 sudo motion update
 bundle install
-#sudo pod setup
+pod repo update --silent
 bundle exec rake pod:install
 bundle exec rake clean
 bundle exec rake spec output=colorized
